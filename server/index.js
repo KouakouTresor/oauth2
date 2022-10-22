c3481136cc065fb4c2529c52a232db3890d8c007
Tresor Kouakou <tresorkouakou@gmail.com>
2022-10-22 22:17:32 +0200

    passport set up

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const passport = require("passport")
+const GoogleStrategy = require("passport-google-oauth20").Strategy
 const app = express();
+const keys = require("./config/keys")
 
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClienSecret,
+    callbackURL: "/auth/google/callback"
+}, (accesToken, refreshToken, profile, done)=>{
+  console.log('access token', accesToken);
+  console.log('refresh token', refreshToken);
+  console.log('profile', profile);}))
 
-app.get("/", (req, res)=>{
-    res.send({name: "Enrico", age: 28})
-})
-
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+  
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
-    console.log("connected to the port 4000")
+    console.log("connected to the port 5000")
 })
